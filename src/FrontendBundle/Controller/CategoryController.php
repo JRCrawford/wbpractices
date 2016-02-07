103bd0425fb4b7b912d8298a07837ca4c5a2c283
John Crawford <j.crawford@wbpractices.com>
2016-02-07 21:12:58 +0100

    WIP: adding article section/update to style

--- src/FrontendBundle/Controller/CategoryController.php
+++ src/FrontendBundle/Controller/CategoryController.php
@@ -14,9 +14,20 @@ class CategoryController extends Controller
      */
     public function indexAction($category)
     {
+        $categoryHeading = "Web Development";
+        $categorySubheading = "Symfony framework will be explained";
+        if ($category == "online-marketing") {
+            $categoryHeading = "Online Marketing";
+            $categorySubheading = "";
+        } else if ($category == "management") {
+            $categoryHeading = "Project Management";
+            $categorySubheading = "";
+        }
         if ($category === "development" || $category === "online-marketing" || $category === "management") {
             return $this->render('category/index.html.twig', array(
-                'category' => $category
+                'category' => $category,
+                'categoryHeading' => $categoryHeading,
+                'categorySubheading' => $categorySubheading
             ));
         } else {
             throw $this->createNotFoundException();
